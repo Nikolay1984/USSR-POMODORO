1c62f2ff00dd972bfe2977d5e9fc7b571735a728
Max <Max@mail.ru>
2020-02-04 21:12:12 +0500

    feature: change packet.json, add gulp css feature

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+var uncss = require('gulp-uncss-sp');
+var csso = require('gulp-csso');
+var csscomb = require('gulp-csscomb');
+ 
+gulp.task('workcss:dev',async function() {
+  return gulp.src('dist/css/*.css')
+    .pipe(csscomb())
+    .pipe(gulp.dest('dist/css/'));
+}); 
 
 
-gulp.task("build",async function () {
-    
-})
-
-gulp.task('watch', function() {
-
+gulp.task('workcss',async function() {
+        gulp.src('dist/css/*.css')
+        .pipe(uncss({
+            html: ['dist/index.html']
+        }))
+        .pipe(autoprefixer())
+        .pipe(csso({
+            debug: true
+        }))
+        .pipe(gulp.dest('app/css'));
 });
+
+
+
+//gulp.task("build",gulp.series('uncss', 'autoprefixer'));
+//
+//gulp.task('watch', async function() {
+//
+//});
