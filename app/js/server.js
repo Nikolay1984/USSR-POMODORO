d78b3ba21f869f08804db98cecb2d887adb7a532
Max <Max@mail.ru>
2020-03-13 09:59:38 +0500

    feature: fix drag and drop

--- app/js/server.js
+++ app/js/server.js
@@ -1,20 +1,17 @@
 "use strict";
 
 var http = require("http");
+var url = require("url");
+
 var server = new http.Server();
 server.listen(1337,"127.0.0.1");
-
-var emit = server.emit;
-
-server.emit = function(event){
-	console.log(event);
-	emit.apply(this, arguments)
-};
 
 
 
 server.on("request",function (req,res) {
-	var result = Math.random();
-	res.end(result + '');
+	var urlObj = url.parse(req.url);
+	console.log(urlObj.query);
+
+	res.end(req.url);
 
 })
