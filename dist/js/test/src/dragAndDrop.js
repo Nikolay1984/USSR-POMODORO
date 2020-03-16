933e46d330d6d9f869ac955b36f17d67b8de841d
Max <Max@mail.ru>
2020-03-16 21:00:21 +0500

    feature: finish scroll bar

--- dist/js/test/src/dragAndDrop.js
+++ dist/js/test/src/dragAndDrop.js
@@ -1,117 +1,56 @@
-
+"use strict";
 
 let ball = document.querySelector(".ball");
-ball.addEventListener("dragstart" , function (e) {
-	e.preventDefault();
+ball.addEventListener("dragstart", function (e) {
+  e.preventDefault();
 });
-ball.addEventListener("mousedown" , function (e) {
-	this.style.zIndex = 1000;
-	document.addEventListener("mousemove" , handlerMousemove);
-	document.addEventListener("mouseup" , function (e) {
-		document.removeEventListener("mousemove" , handlerMousemove);
-	} , {
-		once: true
-	});
+ball.addEventListener("mousedown", function (e) {
+  let handlerMousemoveBind = handlerMousemove.bind(this);
+  document.addEventListener("mousemove", handlerMousemoveBind);
+  document.addEventListener("mouseup", function (e) {
+    document.removeEventListener("mousemove", handlerMousemoveBind);
+  }, {
+    once: true
+  });
 });
 
-function handlerMousemove(e) {}
+function handlerMousemove(e) {
+  let ball = this;
+  let mouseCords = {
+    x: e.clientX,
+    y: e.clientY
+  };
+  let parent = this.offsetParent;
+  let parentRadius = parent.offsetWidth / 2;
+  let ballCords = {
+    offsetLeft: ball.offsetLeft,
+    offsetTop: ball.offsetTop,
+    viewX: ball.getBoundingClientRect().x,
+    viewY: ball.getBoundingClientRect().y,
+    radius: ball.offsetWidth / 2
+  };
+  let innerRadius = parentRadius - ballCords.radius;
+  let mouseCordsDecard = {
+    x: mouseCords.x - ballCords.viewX + ballCords.offsetLeft - parentRadius,
+    y: mouseCords.y - ballCords.viewY + ballCords.offsetTop - parentRadius
+  };
+  let mouseCordsPolar = {
+    angle: Math.atan(mouseCordsDecard.y / mouseCordsDecard.x)
+  };
 
-function moveAt(e , elem) {
-	// let ballOffsetLeft = elem.offsetLeft;
-	// let ballOffsetTop = elem.offsetTop;
-	// let ballRadius = elem.offsetWidth / 2;
-	//
-	// let radiusParentBig = elem.offsetParent.offsetWidth / 2;
-	// let radiusParentSmall = radiusParentBig - (ballRadius * 2);
-	//
-	// let centerBallX = ballRadius + ballOffsetLeft;
-	// let centerBallY = ballRadius + ballOffsetTop;
-	//
-	// let mouseClickOfViewX = e.clientX;
-	// let mouseClickOfViewY = e.clientY;
-	// let offsetParentViewX = elem.offsetParent.getBoundingClientRect().x;
-	// let offsetParentViewY = elem.offsetParent.getBoundingClientRect().y;
-	// let newCenterX = mouseClickOfViewX - offsetParentViewX;
-	// let newCenterY = mouseClickOfViewY - offsetParentViewY;
-	// let finalX;
-	// let finalY;
-	//
-	// if (newCenterY < centerBallY) {
-	// 	// нужно отнимать
-	// 	finalY = ballOffsetTop - (centerBallY - newCenterY);
-	// } else if (newCenterY > centerBallY) {
-	// 	// нужно пребавлять
-	// 	finalY = ballOffsetTop + (newCenterY - centerBallY);
-	// } else {
-	// 	let arrfinalY = elem.style.top.split("");
-	// 	arrfinalY.splice(arrfinalY.length - 2 , 2);
-	// 	finalY = Number(arrfinalY.join(""));
-	// }
-	//
-	// if (newCenterX < centerBallX) {
-	// 	finalX = ballOffsetLeft - (centerBallX - newCenterX);
-	// } else if (newCenterX > centerBallX) {
-	// 	finalX = ballOffsetLeft + (newCenterX - centerBallX);
-	// } else {
-	// 	let arrfinalX = elem.style.left.split("");
-	// 	arrfinalX.splice(arrfinalX.length - 2 , 2);
-	// 	finalX = Number(arrfinalX.join(""));
-	// }
-	// function offset() {
-	//
-	//
-	// 	let newCenterBallX = finalX + ballRadius;
-	// 	let newCenterBallY = finalY + ballRadius;
-	// 	let bigHorizonCathet;
-	// 	let bigVerticalCathet;
-	//
-	// 	if(newCenterBallX < radiusParentBig){
-	// 		bigHorizonCathet = radiusParentBig - newCenterBallX;
-	// 	} else if(newCenterBallX > radiusParentBig){
-	// 		bigHorizonCathet = radiusParentBig - ((radiusParentBig*2) - newCenterBallX);
-	// 	}
-	//
-	// 	if(newCenterBallY < radiusParentBig){
-	// 				bigVerticalCathet = radiusParentBig - newCenterBallY;
-	// 	}else if(newCenterBallY > radiusParentBig){
-	// 			bigVerticalCathet = radiusParentBig - ((radiusParentBig*2) - newCenterBallY);
-	// 	}
-	//
-	// 	let bigHypotenuse = Math.sqrt(Math.pow(bigHorizonCathet,2) + Math.pow(bigVerticalCathet,2));
-	// 	console.log(bigHypotenuse,bigHorizonCathet,bigVerticalCathet);
-	// 	let sinusAlfa = bigHorizonCathet /  bigHypotenuse;
-	// 	let smallHypotenuse = Math.abs(radiusParentSmall - bigHypotenuse);
-	// 	let smallHorizonCathet = sinusAlfa * smallHypotenuse;
-	// 	let smallVerticalCathet = Math.sqrt(Math.pow(smallHypotenuse,2) - Math.pow(smallHorizonCathet,2));
-	//
-	// 	if(bigHypotenuse > radiusParentSmall){
-	// 	finalY = finalY - (smallVerticalCathet - ballRadius );
-	// 	finalX = finalX - (smallHorizonCathet - ballRadius );
-	// 	}else if(bigHypotenuse < radiusParentSmall){
-	// 		finalY = finalY + (smallVerticalCathet - ballRadius );
-	// 		finalX = finalX + (smallHorizonCathet - ballRadius );
-	// 	}
-	//
-	// }
-	//
-	//
-	//  function offset(x,y ){
-	// 	 let horizonCathet;
-	// 	 let verticalCathet;
-	// 	 let hypotenuse;
-	// 		verticalCathet = Math.abs(centerCircleMiddleToOffsetParentY - centerBallY);
-	// 		horizonCathet  = Math.abs(centerCircleMiddleToOffsetParentX - centerBallX);
-	// 		hypotenuse = Math.sqrt((horizonCathet * horizonCathet) + (verticalCathet * verticalCathet));
-	// 		let ang = verticalCathet/hypotenuse;
-	// 		let smallHypotenuse = Math.abs(hypotenuse - circleMiddleRadius);
-	// 		let newKatVertical = smallHypotenuse * ang;
-	// 	 let newKatHorizon = Math.sqrt((smallHypotenuse*smallHypotenuse) - (newKatVertical*newKatVertical));
-	// 	 finalY = centerBallY + newKatVertical;
-	// 	 finalX = centerBallX +  newKatHorizon;
-	//
-	//
-	//  }
-	//  offset();
-	elem.style.left = finalX + "px";
-	elem.style.top = finalY + "px";
+  if (mouseCordsDecard.x < 0) {
+    mouseCordsPolar.angle = mouseCordsPolar.angle + Math.PI;
+  }
+
+  let targetCordsDecard = {
+    x: innerRadius * Math.cos(mouseCordsPolar.angle),
+    y: innerRadius * Math.sin(mouseCordsPolar.angle)
+  };
+  let targetCordsOffset = {
+    x: parentRadius + targetCordsDecard.x - ballCords.radius,
+    y: parentRadius + targetCordsDecard.y - ballCords.radius
+  };
+  console.log(targetCordsDecard);
+  ball.style.left = targetCordsOffset.x + "px";
+  ball.style.top = targetCordsOffset.y + "px";
 }
